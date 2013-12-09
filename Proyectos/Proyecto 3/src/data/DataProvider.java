8c066ccfe04cb0a6d841d76b879f1a2f712bc728
kybalion <psolombrino_9@hotmail.com>
2013-12-08 18:44:32 -0600

    DataProvider.

--- Proyectos/Proyecto 3/src/data/DataProvider.java
+++ Proyectos/Proyecto 3/src/data/DataProvider.java
@@ -4,16 +4,16 @@ import data.DB;
 
 import java.util.Stack;
 
+import entities.IncomingEmail;
 import entities.User;
 import entities.Email;
 
 public class DataProvider {
 	private static final String USER_LOGIN = "SELECT ID, USERNAME, PASSWORD FROM USERS WHERE USERNAME =";
-//    private static final String USER_LOGIN_KEY = "USER_LOGIN";
-    private static final String USER_CONTACTS = "SELECT USERNAME FROM CONTACTS WHERE USER_ID = ";
-//    private static final String USER_CONTACTS_KEY = "USER_CONTACTS";
-    private static final String USER_MAILS = "SELECT SUBJECT, BODY, SENTBY FROM EMAILS WHERE READ = 0 AND TO_USER_ID = ";
-//    private static final String USER_MAILS_KEYS = "USER_MAIL";
+  private static final String USER_CONTACTS = "SELECT USERNAME FROM CONTACTS WHERE USER_ID = ";
+  	private static final String USER_MAILS = "SELECT SUBJECT, BODY, SENTBY FROM EMAILS WHERE READ = 0 AND TO_USER_ID = ";
+	private static final String CHECK_CONTACT = "SELECT ID,USERNAME FROM USERS WHERE USERNAME =";
+    private static final String INSERT_MAIL = "INSERT INTO IncomingMails (Body,Subject,From_Username,Read,Received_On,to_user_id) VALUES ";
     
     DB connection;
     User connectedUser;
@@ -34,8 +34,8 @@ public class DataProvider {
 		String[] data = message.split(" ");
 		try {
 			connection.connect();
-			connection.executeQuery(USER_LOGIN + "'" + data[1] + "'");
-			if(connection.next()) {
+			connection.executeQuery(USER_LOGIN+"'"+data[1]+"'");
+			if(connection.next()){
 				if (data[1].equals(connection.getString("USERNAME"))) {
 					connectedUser = new User();
 					connectedUser.setUserName((String) connection.getString("USERNAME"));
@@ -45,7 +45,6 @@ public class DataProvider {
 			}
 			connection.close();
 		} catch (Exception e) {
-			System.out.println(e);
 			System.out.println("Ocurrio un error al tratar de conectarse.");
 		}
 		return "LOGIN ERROR 101";
@@ -95,4 +94,57 @@ public class DataProvider {
 		}
     	return null;
     }
+	
+	public String ServerIncommingEmail(String mailTo, String mailFrom, String mailSubject, String mailBody) {
+		String[] to = mailTo.split(" ");
+		String[] from = mailFrom.split(" ");
+		String[] subject = mailSubject.split(" ");
+		String[] body = mailBody.split(" ");
+		try {
+			connection.connect();
+			connection.executeQuery(CHECK_CONTACT+"'"+to[2]+"'");
+			if(connection.next()){
+				IncomingEmail newEmail = new IncomingEmail (subject[2],body[2],from[2],(long)(Integer.parseInt((String) connection.getString("ID"))));
+				connection.close();
+				connection.connect();
+				System.out.println(INSERT_MAIL+"('"+newEmail.getBody()+"','"+newEmail.getSubject()+"','"+newEmail.getSentBy()+"',"+newEmail.getRead()+",'"+newEmail.getReceivedOn()+"',"+newEmail.getToUserID()+")");
+				connection.executeNonQuery(INSERT_MAIL+"('"+newEmail.getBody()+"','"+newEmail.getSubject()+"','"+newEmail.getSentBy()+"',"+newEmail.getRead()+",'"+newEmail.getReceivedOn()+"',"+newEmail.getToUserID()+")");
+				connection.close();
+				return "OK SEND MAIL";
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("Ocurrio un error al tratar de conectarse.");
+		} finally {
+			try {
+				connection.close();
+			} catch (UGDBDisconnectException e) {
+				e.printStackTrace();
+			}
+		}
+		return "SEND ERROR 201 "+to[2];
+	}
+	
+	public String ServerCheckContact(String message) {
+		String[] data = message.split(" ");
+		
+		try {
+			connection.connect();
+			connection.executeQuery(CHECK_CONTACT+"'"+data[2]+"'");
+			if(connection.next()){
+				return "OK CHECK CONTACT "+connection.getString("USERNAME");
+			}
+			connection.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("Ocurrio un error al tratar de conectarse.");
+		} finally {
+			try {
+				connection.close();
+			} catch (UGDBDisconnectException e) {
+				e.printStackTrace();
+			}
+		}
+		return "CHECK ERROR 205";
+	}
 }
